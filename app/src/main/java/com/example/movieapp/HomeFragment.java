e7bd734d1c127189cf3f097748197126aa287fad
rahul-53 <yrahul0620@gmail.com>
2021-09-13 15:12:26 +0530

    third commit

--- app/src/main/java/com/example/movieapp/HomeFragment.java
+++ app/src/main/java/com/example/movieapp/HomeFragment.java
@@ -2,20 +2,45 @@ package com.example.movieapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 public class HomeFragment extends Fragment {
 
+    private ImageView mIvMovieImg;
+
+   private FragmentManager fragmentManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initView(view);
+    }
+
+
+
+    private void initView(View view){
+        mIvMovieImg = view.findViewById(R.id.ivMovieImg);
+        mIvMovieImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
