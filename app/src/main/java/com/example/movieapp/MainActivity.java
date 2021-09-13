d8eebebafbe80c80edf5487289cd3ae692c55e04
rahul-53 <yrahul0620@gmail.com>
2021-09-13 16:17:28 +0530

    sixth commit

--- app/src/main/java/com/example/movieapp/MainActivity.java
+++ app/src/main/java/com/example/movieapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.movieapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Adapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    private void createView(){
+        //RecyclerView.Adapter adapter ;
+        //LinearLayoutManager linearLayoutManager = new LinearLayoutManager();
     }
 }
--- app/src/main/java/com/example/movieapp/MovieDetailsFragment.java
+++ app/src/main/java/com/example/movieapp/MovieDetailsFragment.java
@@ -2,18 +2,50 @@ package com.example.movieapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MovieDetailsFragment extends Fragment {
+    private TextView mTvMovieTitle, mTvMovieDescription, mTvMovieReleaseDate;
+    private ImageView mIvMovieDetailsImage;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_movie_details, container, false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initView(view);
+
+    }
+    private  void initView(View view){
+        mIvMovieDetailsImage = view.findViewById(R.id.ivMovieDetailsImg);
+        mTvMovieTitle = view.findViewById(R.id.tvMovieTitle);
+        mTvMovieReleaseDate = view.findViewById(R.id.tvReleaseDate);
+        mTvMovieDescription = view.findViewById(R.id.tvMovieDescription);
+
+        mIvMovieDetailsImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
